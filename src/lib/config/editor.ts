ea5d1fef13bc7de42954292ac354ab1beece1e85
peterpeterparker <david.dalbusco@outlook.com>
2022-05-11 18:18:54 +0200

    feat: delete image and code paragraphs

--- src/lib/config/editor.ts
+++ src/lib/config/editor.ts
@@ -6,10 +6,11 @@ import {imgUnsplash} from '$lib/plugins/img.unsplash.plugin';
 import type {StyloConfig} from '@papyrs/stylo';
 import {h1, h2, h3, hr, ul} from '@papyrs/stylo/dist/stylo/index.esm';
 import {dirtyAttributes} from '@deckdeckgo/editor';
+import {imgMenu} from '../menus/img.menu';
 
 export const editorConfig: Partial<StyloConfig> = {
   plugins: [h1, h2, h3, ul, imgStorage, imgUnsplash, imgTenor, code, hr],
-  menus: [codeMenu],
+  menus: [codeMenu, imgMenu],
   attributes: {
     paragraphIdentifier: 'paragraph_id',
     exclude: dirtyAttributes
--- src/lib/menus/code.menu.ts
+++ src/lib/menus/code.menu.ts
@@ -1,18 +1,20 @@
 import {openEditCodeModal} from '$lib/utils/code.utils';
 import type {StyloMenu} from '@papyrs/stylo';
+import {deleteMenuAction} from '../utils/menu.utils';
 
+// Svg: https://fonts.google.com/icons?selected=Material%20Icons%20Sharp%3Aedit%3A
 export const codeMenu: StyloMenu = {
   match: ({paragraph}: {paragraph: HTMLElement}) =>
     paragraph?.nodeName.toLowerCase() === 'deckgo-highlight-code',
   actions: [
     {
       text: 'edit_code',
-      icon: `<svg xmlns="http://www.w3.org/2000/svg" width='20' height='20' viewBox="0 0 512 512">
-                <path d="M384 224v184a40 40 0 01-40 40H104a40 40 0 01-40-40V168a40 40 0 0140-40h167.48" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/>
-                <path d="M459.94 53.25a16.06 16.06 0 00-23.22-.56L424.35 65a8 8 0 000 11.31l11.34 11.32a8 8 0 0011.34 0l12.06-12c6.1-6.09 6.67-16.01.85-22.38zM399.34 90L218.82 270.2a9 9 0 00-2.31 3.93L208.16 299a3.91 3.91 0 004.86 4.86l24.85-8.35a9 9 0 003.93-2.31L422 112.66a9 9 0 000-12.66l-9.95-10a9 9 0 00-12.71 0z"/>
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor">
+                <path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM21.41 6.34l-3.75-3.75-2.53 2.54 3.75 3.75 2.53-2.54z"/>
              </svg>`,
       action: async ({paragraph}: {paragraph: HTMLElement}) =>
         openEditCodeModal({highlightCodeElement: paragraph})
-    }
+    },
+    deleteMenuAction({text: 'delete_code'})
   ]
 };
