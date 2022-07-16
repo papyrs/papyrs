072a3eeac985a87214548253dc78339a75027912
David Dal Busco <david.dalbusco@outlook.com>
2022-07-16 15:49:20 +0200

    feat: keep track of last language and options of code (#31)

--- src/lib/utils/code.utils.ts
+++ src/lib/utils/code.utils.ts
@@ -1,6 +1,8 @@
+import {browser} from '$app/env';
 import type {LanguageCode} from '$lib/types/code';
 import type {DeckdeckgoHighlightCodeLanguage} from '@deckdeckgo/highlight-code';
 import {deckdeckgoHighlightCodeLanguages} from '@deckdeckgo/highlight-code/dist/deckdeckgo-highlight-code/index.esm';
+import type {MonacoEditorOptions} from '@deckdeckgo/monaco-editor';
 
 export const codeLanguages = (): LanguageCode[] => {
   const languages: LanguageCode[] = [];
@@ -16,3 +18,18 @@ export const codeLanguages = (): LanguageCode[] => {
 
   return languages;
 };
+
+export const codeOptions = (): MonacoEditorOptions => {
+  const {code_options: codeOptions}: Storage = browser
+    ? localStorage
+    : ({code_options: undefined} as unknown as Storage);
+
+  if (codeOptions === undefined) {
+    return {language: 'javascript', lineNumbers: 'off'};
+  }
+
+  return JSON.parse(codeOptions);
+};
+
+export const saveCodeOptions = (options: MonacoEditorOptions) =>
+  localStorage.setItem('code_options', JSON.stringify(options));
