b9223598edc2caa16dca1075e7963e505ed0ca5e
David Dal Busco <david.dalbusco@outlook.com>
2022-09-24 17:43:40 +0200

    feat: move avatar to ui
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/lib/components/core/Avatar.svelte
+++ src/lib/components/core/Avatar.svelte
@@ -1,19 +1,7 @@
 <script lang="ts">
   import {user} from '$lib/stores/user.store';
-  import {IconUser} from '@papyrs/ui';
+  import {Avatar} from '@papyrs/ui';
   import {i18n} from '$lib/stores/i18n.store';
 </script>
 
-{#if $user.photoUrl}
-  <img loading="lazy" src={$user.photoUrl} alt={$i18n.tools.profile_image} />
-{:else}
-  <IconUser />
-{/if}
-
-<style lang="scss">
-  img {
-    width: 24px;
-    min-height: 24px;
-    border: 1px solid black;
-  }
-</style>
+<Avatar photoUrl={$user.photoUrl} alt={$i18n.tools.profile_image} />
