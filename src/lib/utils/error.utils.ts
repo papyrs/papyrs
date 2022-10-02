41df1b8d110db66b52346b3a9ac93af29e7b3e90
David Dal Busco <david.dalbusco@outlook.com>
2022-10-02 13:03:56 +0200

    chore: remove no more used
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/lib/utils/error.utils.ts
+++
@@ -1,6 +0,0 @@
-export const errorDetailToString = (err: unknown): string | undefined =>
-  typeof err === 'string'
-    ? (err as string)
-    : err instanceof Error
-    ? (err as Error).message
-    : undefined;
