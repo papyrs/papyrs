72ce6941d002956f88a82e664f592be031a2975b
David Dal Busco <david.dalbusco@outlook.com>
2022-10-11 21:35:23 +0200

    feat: custom layout
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ src/routes/proxy/signin/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import {Toasts} from '@papyrs/ui';
+    import {i18n} from '$lib/stores/i18n.store';
+</script>
+
+{#await i18n.init() then}
+    <slot />
+
+    <Toasts />
+{/await}
+
+<style lang="scss" global>
+  @use '../../../../node_modules/@papyrs/ui/styles/mixins/main';
+
+  @import '@papyrs/ui/styles/global.scss';
+
+  :root {
+    @include main.layout;
+  }
+</style>
